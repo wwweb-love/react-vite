f53ff2efb48a1ce887314e365878b910f9cf8051
Roman <rroman2115@yandex.ru>
2025-02-16 18:18:52 +0400

    Edit App.jsx 1.1.1

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,40 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, createElement } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
+export function App() {
 	// Декларативный
-	const date = new Date(Date.now())
+	const date = new Date(Date.now());
 
-	// Декларативный
-	const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
 	// Императивный
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					{date.getFullYear()}
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	)
-}
+	return createElement(
+		"div",
+		{},
+		createElement(
+			"div",
+			{},
+			createElement(
+				"a",
+				{
+					href: "https://vite.dev",
+					target: "_blank",
+				},
+				createElement(
+					"img",
+					{
+						src: viteLogo,
+						className: "logo",
+						alt: "Vite logo",
+					}
+				)
+			),
+			createElement(
+				"a",
+				{
+					href: "https://react.dev",
+					target: "_blank",
+				},
+				createElement(
+					"img",
+					{
+						src: reactLogo,
+						className: "logo react",
+						alt: "React logo"
+					}
+				)
+			)
+		),
+		createElement(
+			"h1",
+			{},
+			"Vite + React"
+		),
 
-export default App
+		createElement(
+			"div",
+			{
+				className: "card"
+			},
+			'',
+			createElement(
+				"button",
+				// {},
+				{ onClick: () => setCount((count) => count += 1) },
+				// "count EDIT"
+				`count is ${count}`
+			),
+			createElement(
+				"p",
+				{},
+				`${date.getFullYear()}`,
+				// "EDIT Date time"
+			)
+		),
+		createElement(
+			"p",
+			{
+				className: "read-the-docs"
+			},
+			"Click on the Vite and React logos to learn more"
+		)
+
+	)
+
+}
